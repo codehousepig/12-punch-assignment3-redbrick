91150e7c2c384a917ba929cfd76c50fcbc270a75
Eugene Choi <42341135+n12seconds@users.noreply.github.com>
2021-11-09 08:08:34 +0900

    Modify : gitignore수정, src폴더로 app.js, bin 이동, app.js  설정 추가

---
+++ src/app.js
@@ -0,0 +1,37 @@
+import createError from "http-errors";
+import express from "express";
+import path from "path";
+import cookieParser from "cookie-parser";
+import logger from "morgan";
+import indexRouter from "./routes/index";
+import usersRouter from "./routes/users";
+// import jwtMiddleware from "./middlewares/jwt.middleware";
+import cors from "cors";
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  })
+);
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404, 'Not Found'));
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  next(createError(500, 'Internal Server Error'));
+});
+
+module.exports = app;
